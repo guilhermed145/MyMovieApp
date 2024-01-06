f6d5cd86129ea2189e2bf7d45008ef00affa72da
guilhermed145 <guilhermed1506@gmail.com>
2024-01-06 09:19:53 -0300

    Make a function to bind data to a MovieListView

--- app/src/main/java/com/myportfolio/mymovieapp/ui/MovieListView.kt
+++ app/src/main/java/com/myportfolio/mymovieapp/ui/MovieListView.kt
@@ -1,14 +1,20 @@
 package com.myportfolio.mymovieapp.ui
 
 import android.content.Context
-import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.myportfolio.mymovieapp.R
 
-class MovieListView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class MovieListView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
-    var movieListAdapter = 0
+    fun loadList(title: String, adapter: MovieListAdapter, onClick: ((View) -> Unit)) {
+        findViewById<TextView>(R.id.category_title)?.text = title + " >"
+        findViewById<RecyclerView>(R.id.home_recycler_view).adapter = adapter
+        setOnClickListener(onClick)
+    }
 
 
 }
